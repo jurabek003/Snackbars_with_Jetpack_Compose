e06e36bbfe131074a47d6d7c3446004a1ff0a1e6
Asus <turgunboyevjurabek777@gmail.com>
2023-11-29 14:36:37 +0500

    not context

--- app/src/main/java/uz/turgunboyevjurabek/snackbarswithjetpackcompose/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/snackbarswithjetpackcompose/MainActivity.kt
@@ -11,37 +11,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import uz.turgunboyevjurabek.snackbarswithjetpackcompose.ui.theme.SnackbarsWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
             SnackbarsWithJetpackComposeTheme {
-
-
-                    }
-
-
-            }
+                ViewUi()
+             }
+           }
         }
     }
 
@@ -61,8 +55,13 @@ fun ViewUi(){
         mutableStateOf("")
     }
 
-//    val scaffoldState=rememberScaffoldState()
-    Scaffold {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(snackbarHost = {
+        SnackbarHost(hostState = snackbarHostState)
+    },
+        ) {
 
 
         Column(
@@ -80,7 +79,22 @@ fun ViewUi(){
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth())
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { }) {
+            Button(onClick = {
+                scope.launch {
+
+                    val result=snackbarHostState.showSnackbar(message = textfaildState,
+                        actionLabel = "Action",
+                        duration = SnackbarDuration.Short)
+                    when(result){
+                        SnackbarResult.ActionPerformed->{
+                           // Toast.makeText(context, "Assalom alekum", Toast.LENGTH_SHORT).show()
+                        }
+                        SnackbarResult.Dismissed->{
+
+                        }
+                    }
+                }
+            }) {
                 Text(text = "Pls greet me")
             }
         }
